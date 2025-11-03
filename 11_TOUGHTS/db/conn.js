dd520670b2f0856c218f5ad962065f4e24456977
AndreSantosDS <andre8628852@gmail.com>
2025-11-03 13:10:50 -0300

    Finalizado o projeto toughts

---
+++ 11_TOUGHTS/db/conn.js
@@ -0,0 +1,15 @@
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize('toughts2', 'root', '9139', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+try{
+    sequelize.authenticate();
+    console.log('Conexão com o banco de dados realizada com sucesso!');
+}catch(err){
+    console.log('Conexão com o banco de dados falhou:', err);
+}
+
+module.exports = sequelize;
---
+++ 11_TOUGHTS/models/Tought.js
@@ -0,0 +1,18 @@
+const {DataTypes} = require('sequelize');
+const conn = require('../db/conn');
+
+// User
+const User = require('./User');
+
+const Tought = conn.define('Tought', {
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        require: true,
+    },
+});
+
+Tought.belongsTo(User);
+User.hasMany(Tought);
+
+module.exports = Tought;
